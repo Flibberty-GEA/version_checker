031e4d96d0fe15337f0cc4672c83da9b63bafdf0
Eugene Goliuk <yevgeniy.golyuk@gmail.com>
2018-08-05 18:17:11 +0200

    Add main script

---
+++ src/runner.py
@@ -0,0 +1,21 @@
+import sys
+print("Версія Python: {}".format(sys.version))
+
+import pandas as pd
+print("Версія pandas: {}".format(pd.__version__))
+
+import matplotlib
+print("Версія matplotlib: {}".format(matplotlib.__version__))
+
+import numpy as np
+print("Версія NumPy: {}".format(np.__version__))
+
+import scipy as sp
+print("Версія SciPy: {}".format(sp.__version__))
+
+import IPython
+print("Версія IPython: {}".format(IPython.__version__))
+
+import sklearn
+print("Версія scikit-learn: {}".format(sklearn.__version__))
+
